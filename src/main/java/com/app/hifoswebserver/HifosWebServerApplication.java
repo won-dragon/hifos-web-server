6ac86e4d3d9662cd7bdbe6a2a7089a45c7ee9882
wonyong kim <won-dragon@wonyongui-MacBookPro.local>
2021-09-23 17:32:56 +0900

    Initial commit

---
+++ src/main/java/com/app/hifoswebserver/HifosWebServerApplication.java
@@ -0,0 +1,13 @@
+package com.app.hifoswebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HifosWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HifosWebServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/hifoswebserver/HifosWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hifoswebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HifosWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
